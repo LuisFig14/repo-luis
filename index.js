ac6506d6c6943d2cd199666a0303b5195c3230ac
Fernando Licea <fernandolic1407@gmail.com>
2022-10-14 07:45:22 -0500

    Agregando js para trabajo remoto personal

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 console.log("Aprendiendo GIT");
 console.log("Haciendo pruebas por mi cuenta");
+
+
+
+//Arreglos JONMircha
+
+const colores = ["Rojo", "Verde", "Azul"];
+
+
+colores.push("Amarillo"); //Añade un elemento al arreglo
+// console.log(colores);
+
+colores.pop();            //Elimina el ultimo elemento del arreglo
+// console.log(colores);
+
+colores.forEach(function(el){
+    console.log(`<li>${el}>/li>`);
+});
