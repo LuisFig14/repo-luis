a17a0c54e966bbb0d9f91042e72ae2e2eef2a7a9
Fernando Licea <fernandolic1407@gmail.com>
2022-10-15 22:10:31 -0500

    Trabajo del 15/10/2022

--- js/jonmircha.js
+++ js/jonmircha.js
@@ -21,8 +21,8 @@ const b = {};
 // console.log(b);
 
 
-
-
+//Dentro de los objetos las variables se llaman atributos/propiedades
+//A las funciones se les llama métodos.
 
 const jon = {                   //Se crea el objeto
     nombre:"Luis",              //Se pueden guardar strings
@@ -35,13 +35,25 @@ const jon = {                   //Se crea el objeto
         twitter:"@Fer",
         movil: 3141170867   //Numeros
     },
-    saludar:function(){     //Funciones    
+    saludar:function(){     //Funciones  //Métodos 
         console.log("Hola :)");
+    },
+    decirMiNombre:function(){
+        console.log(`Hola me llamo ${this.nombre} ${this.apellido} y tengo ${this.edad} años`);
     }
 }
 
-console.log(jon);
-console.log(jon["nombre"]);
+// console.log(jon);
+console.log(jon["nombre"]);         //Accediendo al atributo
+console.log(jon.apellido);          //Accediendo al atributo
+console.log(jon.pasatiempos[1]);    //Accediendo al arreglo
+console.log(jon.contacto);          //Accediendo al objeto 
+console.log(jon.contacto.twitter);  //Accediendo al objeto y a su cuerpo
+console.log(jon.contacto.movil);
+
+jon.saludar();                       //Mandando llamar a la funcion que esta dentro dl objeto
+
+jon.decirMiNombre();
 
 
 
